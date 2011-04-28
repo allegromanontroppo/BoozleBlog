686b3bee92759230e844b4d3882f0f3b625d3020
Mark Holland <rennerholland@googlemail.com>
2011-04-28 22:47:37 +0100

    link_to layout fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts
 
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable  :registerable,
-  devise :database_authenticatable, 
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable 
+  devise :database_authenticatable,  :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
