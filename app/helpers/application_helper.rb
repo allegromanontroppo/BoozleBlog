c7213c643c8acf3c0094191e9f9849cd8d7c5684
Mark Holland <rennerholland@googlemail.com>
2011-05-15 21:41:40 +0100

    handheld css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def notice
+    
+    unless flash[:notice].blank?
+      
+      content_tag :p, :id => 'notice' do
+        flash[:notice]      
+      end
+    
+    end
+    
+  end
+  
 end
